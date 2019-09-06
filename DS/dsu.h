3fbb2f1ed03c99b69334a2fff905b5d143c5bb0b
Madhur Chauhan <madhur4127@gmail.com>
2019-09-06 09:11:44 +0530

    Added DSU

--- DS/dsu.h
+++ DS/dsu.h
@@ -1,21 +1,23 @@
-typedef struct _dsu{
-  std::vector<int> par, rank;
-  //MEMBER FUNCTIONS
-  _dsu(int n){
-    par.resize(n);
-    rank.resize(n);
-    for(int i=0;i<n;++i) {
-      rank[i]=0;
-      par[i]=i;
-    }
-  }
-  int find(int x){
-    return x==par[x]?x:par[x]=find(par[x]);
-  }
-  void merge(int x, int y){
-    x=find(x), y=find(y);
-    if(x==y) return ;
-    rank[x]>rank[y]?par[y]=x:par[x]=y;
-    if(rank[x]==rank[y]) rank[y]++;
-  }
-} dsu;
+/**
+ * Author: Lukas Polacek
+ * Date: 2009-10-26
+ * License: CC0
+ * Source: folklore
+ * Description: Disjoint-set data structure.
+ * Time: $O(\alpha(N))$
+ */
+struct DSU {
+	std::vector<int> e;
+	DSU(int n) : e(n, -1) {}
+	bool same_set(int a, int b) { return find(a) == find(b); }
+	int size(int x) { return -e[find(x)]; }
+	int find(int x) { return e[x] < 0 ? x : e[x] = find(e[x]); }
+	bool merge(int a, int b) {
+		a = find(a), b = find(b);
+		if (a == b) return false;
+		// make a as parent of b
+		if (e[a] > e[b]) swap(a, b); // size of dsu(a) is -e[a] 
+		e[a] += e[b]; e[b] = a;
+		return true;
+	}
+};
